ad78f55c1e904b62b860ddc322d388d5f46a35bc
Avisek Ray <biisal.int@gmail.com>
2025-03-17 07:15:26 +0530

    undo err

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,17 +1,6 @@
 ---@type ChadrcConfig
 local M = {}
 
--- Path to overriding theme and highlights files
-local highlights = require "custom.highlights"
-
-M.ui = {
-  hl_override = highlights.override,
-  hl_add = highlights.add,
-
-  transparency = true,
-}
-
-M.plugins = require "plugins"
-
-
+M.ui = {transparency= true }
+M.plugins = "custom.plugins"
 return M
